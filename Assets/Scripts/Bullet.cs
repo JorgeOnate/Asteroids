1b347471a2701dec991b48a00f8cef82a22e7662
JorgeOnate <jorge.onate.2017@upb.edu.co>
2020-10-19 21:35:25 -0500

    GameCompleted

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    void Update()
+    {
+        StartGame();
+    }
+    public void StartGame()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
